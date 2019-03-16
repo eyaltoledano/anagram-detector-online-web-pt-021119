000b3a442ba60898982ceb697334ff5449ffd787
Eyal Toledano <eyaltoledano@me.com>
2019-03-16 18:46:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
-# Your code goes here!
+
+require 'pry'
 
 class Anagram
   attr_accessor :word
@@ -9,7 +10,7 @@ class Anagram
 
   def match(word_array)
     word_array.each do |word|
-      
+      binding.pry
     end
   end
 end
